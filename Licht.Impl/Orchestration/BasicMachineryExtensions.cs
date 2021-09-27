e0a10860fd84cd108553b7c10abcb4492ed1ebca
ConradoClarke <conradoclarke@gmail.com>
2021-09-27 09:48:34 -0300

    basic machinery extensions

---
+++ Licht.Impl/Orchestration/BasicMachineryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Licht.Impl.Orchestration
+{
+    public static class BasicMachineryExtensions
+    {
+        public static BasicMachinery AddBasicMachine(this BasicMachinery basicMachinery, int priority, IEnumerable<Action> steps)
+        {
+            basicMachinery.AddMachines(new BasicMachine(priority, steps));
+            return basicMachinery;
+        }
+    }
+}
