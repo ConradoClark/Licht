d2c64caa263b5b1548d90dc05961bf0ff6334089
ConradoClarke <conradoclarke@gmail.com>
2022-04-15 12:20:58 -0300

    Make multiplier from ITime settable

--- Licht.Interfaces/Time/ITime.cs
+++ Licht.Interfaces/Time/ITime.cs
@@ -4,7 +4,7 @@ namespace Licht.Interfaces.Time
 {
     public interface ITime: IUpdateable, IResettable, IActivable, IDeactivable
     {
-        double Multiplier { get; }
+        double Multiplier { get; set; }
         double TotalElapsedTimeInMilliseconds { get; }
         double UpdatedTimeInMilliseconds { get; }
         bool Set(double time);
